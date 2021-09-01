bfd796af564d39a35e140143c9dda29eb3fd1987
arun-karthick-yash <89441141+arun-karthick-yash@users.noreply.github.com>
2021-09-01 16:12:08 +0530

    Update hello.php

--- src/hello.php
+++ src/hello.php
@@ -4,7 +4,7 @@ namespace helloworld;
 class helloworld {
   // Properties
   public $name;
-  public $color;
+  
 
   // Methods
   function set_name($name) {
@@ -13,20 +13,15 @@ class helloworld {
   function get_name() {
     return $this->name;
   }
-  function set_color($color) {
-    $this->color = $color;
-  }
-  function get_color() {
-    return $this->color;
-  }
+  
 }
 
 $apple = new helloworld();
 $apple->set_name('Apple');
-$apple->set_color('Red');
+
 echo "Name: " . $apple->get_name();
-echo "<br>";
-echo "Color: " .  $apple->get_color();
+
+
 ?>
 
 
