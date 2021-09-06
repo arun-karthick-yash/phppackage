d5b5f6d684b631c4a7e15b96afcf5b05fdefc826
arun-karthick-yash <89441141+arun-karthick-yash@users.noreply.github.com>
2021-09-06 19:32:49 +0530

    Update RandomQuotes.php

--- src/RandomQuotes.php
+++ src/RandomQuotes.php
@@ -5,7 +5,7 @@
   class RandomQuotes {
 
     public function getRandomQuotes() {
-
+    /*
       // Get the list of quotes.
       $quotes = file_get_contents( __DIR__ . '/../inc/quotes.json' );
       // Convert JSON document to PHP array.
@@ -13,7 +13,9 @@
       // Get a random index number.
       $index = mt_rand( 0, count( $quotes ) );
       // Return the random quote.
-      return $quotes[ $index ];
+      return $quotes[ $index ]; */
+      
+      return $msg = "Composer Package";
 
     }
 
